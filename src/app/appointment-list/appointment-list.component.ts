7c034aff64b9358130b76901b2552dd1a0462645
takano_shaft <goyataiko@gmail.com>
2023-12-01 17:27:13 +0900

    interface

--- src/app/appointment-list/appointment-list.component.ts
+++ src/app/appointment-list/appointment-list.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { Appointment } from '../models/appointment';
 
 @Component({
   selector: 'app-appointment-list',
   templateUrl: './appointment-list.component.html',
   styleUrls: ['./appointment-list.component.css']
 })
-export class AppointmentListComponent {
 
+export class AppointmentListComponent {
+  appointment: Appointment = {
+    id: 1,
+    title: "Take dog for a walk",
+    date: new Date("2023-12-01")
+  }
 }
