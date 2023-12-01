a1007ff8206e30e8f7026b970613a256f3735882
takano_shaft <goyataiko@gmail.com>
2023-12-01 17:50:48 +0900

    click event

--- src/app/appointment-list/appointment-list.component.ts
+++ src/app/appointment-list/appointment-list.component.ts
@@ -12,4 +12,8 @@ export class AppointmentListComponent {
   newAppointmentDate : Date = new Date;
   
   appointment: [] =[]
+
+  addAppointment(){
+    alert(this.newAppointmentTitle + "   " + this.newAppointmentDate)
+  }
 }
