171705a668b67a542c83fbb9068ae9cb04dc1637
Takano Computer <goyataiko@gmail.com>
2023-12-03 20:16:47 +0900

    indent 수정

--- src/app/appointment-list/appointment-list.component.ts
+++ src/app/appointment-list/appointment-list.component.ts
@@ -2,37 +2,37 @@ import { Component } from '@angular/core';
 import { Appointment } from '../models/appointment';
 
 @Component({
-  selector: 'app-appointment-list',
-  templateUrl: './appointment-list.component.html',
-  styleUrls: ['./appointment-list.component.css']
+   selector: 'app-appointment-list',
+   templateUrl: './appointment-list.component.html',
+   styleUrls: ['./appointment-list.component.css']
 })
 
 export class AppointmentListComponent {
-  newAppointmentTitle : string = "";
-  newAppointmentDate : Date = new Date;
-  
-  appointments: Appointment[] =[]
+   newAppointmentTitle: string = "";
+   newAppointmentDate: Date = new Date;
 
-  addAppointment(){
-    //스페이스를 모두 뺸것의 길이가 0이 아님,      날짜가 존재함
-    if(this.newAppointmentTitle.trim().length && this.newAppointmentDate != null ){
-      let newAppointment : Appointment = {
-        id: Date.now(),
-        title: this.newAppointmentTitle,
-        date: this.newAppointmentDate
+   appointments: Appointment[] = []
+
+   addAppointment() {
+      //스페이스를 모두 뺸것의 길이가 0이 아님,      날짜가 존재함
+      if (this.newAppointmentTitle.trim().length && this.newAppointmentDate != null) {
+         let newAppointment: Appointment = {
+            id: Date.now(),
+            title: this.newAppointmentTitle,
+            date: this.newAppointmentDate
+         }
+         this.appointments.push(newAppointment)
+         this.newAppointmentTitle = "";
+         this.newAppointmentDate = new Date();
+
+         //local에 저장하기, stringify하여 저장
+         localStorage.setItem("appointments", JSON.stringify(this.appointments))
       }
-      this.appointments.push(newAppointment)
-      this.newAppointmentTitle="";
-      this.newAppointmentDate=new Date();
+   }
 
+   deleteAppointment(index: number) {
+      this.appointments.splice(index, 1);
       //local에 저장하기, stringify하여 저장
       localStorage.setItem("appointments", JSON.stringify(this.appointments))
-    }
-  }
-
-  deleteAppointment(index: number){
-    this.appointments.splice(index, 1);
-    //local에 저장하기, stringify하여 저장
-    localStorage.setItem("appointments", JSON.stringify(this.appointments))
-  }
+   }
 }
